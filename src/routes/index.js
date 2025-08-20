07c33ab9883e413905fd6ab9c178410bb6324dce
yutian81 <yutian88881@hotmail.com>
2025-08-20 18:15:25 +0800

    Delete src directory

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-export { handleAuthRequest } from './auth.js';
-export { handleLoginRequest } from './login.js';
-export { handleUploadRequest } from './upload.js';
-export { handleAdminRequest } from './admin.js';
-export { handleDeleteRequest } from './delete.js';
-export { handleSearchRequest } from './search.js';
-export { handleBingImagesRequest } from './bing.js';
