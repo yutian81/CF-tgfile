1365e49e638fb7786ff7af201e3ce18330638ab4
yutian81 <yutian88881@hotmail.com>
2025-08-28 17:21:47 +0800

    Delete components directory

--- components/copyright.js
+++
@@ -1,9 +0,0 @@
-export const copyright = `
-  <p>
-    <span><i class="fas fa-copyright"></i> 2025 Copyright by Yutian81</span><span>|</span>
-    <a href="https://github.com/yutian81/CF-tgfile" target="_blank">
-    <i class="fab fa-github"></i> GitHub Repo</a><span>|</span>
-    <a href="https://blog.811520.xyz/" target="_blank">
-    <i class="fas fa-blog"></i> 青云志博客</a>
-  </p>
-`;
